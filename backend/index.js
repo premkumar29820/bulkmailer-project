81c7666c9116b330118b489e0bb65cc4229d7eb3
PREMKUMAR <167052615+premkumar29820@users.noreply.github.com>
2026-09-17 01:10:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -169,18 +169,4 @@ app.get("/api/emails", authMiddleware, async (req, res) => {   //history fetch a
 });
 
 
-const PORT = process.env.PORT || 3000;
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected successfully");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("MongoDB connection failed:");
-    console.error(error);
-  });
+module.exports = app;
